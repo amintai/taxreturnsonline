9319d9b2f72cfc1ff39f5472f8cf4daaa59add69
AminTaiTntra <amin.tai@tntra.io>
2025-04-09 16:04:15 +0530

    Services import issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import Services from "./pages/Services";
 import CompanyRegistration from "./pages/Services/companyRegistration";
 import GstRegistration from "./pages/Services/GST";
 import CustomMarquee from "./components/Marquee";
 import ImportExportCode from "./pages/Services/ImportExportCode";
 import Header from "./components/Header";
 import { Analytics } from "@vercel/analytics/react"
+import Services from "./pages/Services"
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
     <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
+        <Route path="/our-services" element={<Services />} />
         <Route path="/our-services/company-registration" element={<CompanyRegistration />} />
         <Route path="/our-services/gst-registration" element={<GstRegistration />}/>
         <Route path="/our-services/iec-registration" element={<ImportExportCode />}/>
